cfd98be5dd41754883b5331bdd0a0482a37afafb
Rafiul27 <rafiulhuda01@gmail.com>
2023-10-19 16:15:58 +0700

    update Tugas pertemuan 4

--- Tugas/index3.js
+++ Tugas/index3.js
@@ -1,34 +1,48 @@
-const readline = require('readline')
-const validator = require('validator')
-
+const readline = require('readline');
+const validator = require('validator');
 
 const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
-function input(){
-    rl.question("Masukkan nama anda ? ", (nama) => {
-        rl.question("Masukkan Nomor Handphone anda ? ", (handphone) => {
-            if(validator.isMobilePhone(handphone, "any")){
-                rl.question("Masukkan email anda ? ", (email) => {
-                    if(validator.isEmail(email)){
-                        console.log(`Nama saya adalah ${nama}, ` + `nomor telepon saya ${handphone}, `  + `dan email saya adalah ${email}, Thank You!!`,);
-                        rl.close();
-                    }else{
-                        console.log(
-                            "Email is Invalid, try entering your email again!!!"
-                        ),
-                        input();
-                    }
-                });
-            }else{
-                console.log(
-                    "Number is Invalid, try entering your number again!!!"
-                ),
-                input();
-            }
-        })
-    })
+let nama = '';
+let handphone = '';
+
+function inputNama() {
+    rl.question("Masukkan nama anda ? ", (inputNama) => {
+        if (validator.isLength(inputNama, { min: 3, max: 50 })) {
+            nama = inputNama;
+            inputNomorHandphone();
+        } else {
+            console.log("Nama harus memiliki panjang minimal 3 karakter dan maksimal 50 karakter. Silakan coba lagi.");
+            inputNama();
+        }
+    });
 }
-input();
+
+function inputNomorHandphone() {
+    rl.question("Masukkan Nomor Handphone anda ? ", (inputHandphone) => {
+        if (validator.isMobilePhone(inputHandphone, "any")) {
+            handphone = inputHandphone;
+            inputEmail();
+        } else {
+            console.log("Nomor Handphone tidak valid. Silakan coba lagi.");
+            inputNomorHandphone();
+        }
+    });
+}
+
+function inputEmail() {
+    rl.question("Masukkan email anda ? ", (email) => {
+        if (validator.isEmail(email)) {
+            console.log(`Nama saya adalah ${nama}, nomor telepon saya ${handphone}, dan email saya adalah ${email}. Thank You!!`);
+            rl.close();
+        } else {
+            console.log("Alamat email tidak valid. Silakan coba lagi.");
+            inputEmail();
+        }
+    });
+}
+
+inputNama();
